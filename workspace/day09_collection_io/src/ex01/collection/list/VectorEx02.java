01ee1be70c8bc7cf3ddb086e8c328636be98b0d8
BeJ Park <bjpark0506@naver.com>
2021-09-02 09:49:57 +0900

    Indexing

---
+++ workspace/day09_collection_io/src/ex01/collection/list/VectorEx02.java
@@ -0,0 +1,62 @@
+package ex01.collection.list;
+
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class VectorEx02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		Date date = new Date();
+		Vector v = new Vector(3,5);
+		
+		v.addElement("java");
+		v.addElement(new Double(10.2));
+		v.addElement(date);
+		System.out.println(v.size()+", "+v.capacity());
+		
+		for(i=0;i<10;i++)
+			v.addElement(new Integer(i));
+		
+		System.out.println(v.size()+", "+v.capacity());
+		
+		
+		Enumeration enu = v.elements();
+		while(enu.hasMoreElements())
+			System.out.print(enu.nextElement()+"\t");
+		
+		
+		System.out.println("객체 내용 포함 확인");
+		
+		if(v.contains("java")) System.out.println("yes");
+		else System.out.println("no");
+		
+		System.out.println("10.2 객체 위치 : "+v.indexOf(new Double(10.2)));
+		System.out.println("입력 시간 : "+date);
+		System.out.println(v.get(v.indexOf(date)));
+		
+		//삭제
+		System.out.println(v.size()+", "+v.capacity());
+		System.out.println(v);
+		for(i=0;i<v.size();i++) 
+			v.removeElementAt(i);
+		
+		System.out.println(v.size()+", "+v.capacity());
+		System.out.println(v);
+		v.setElementAt("kkk", 2);
+		System.out.println(v);
+		v.trimToSize();
+		System.out.println(v.size()+", "+v.capacity());
+		v.setSize(2);
+		System.out.println(v);
+		System.out.println(v.size()+", "+v.capacity());
+
+
+
+
+		
+	}//main
+
+}
