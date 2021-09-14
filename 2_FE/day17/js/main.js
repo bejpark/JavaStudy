9151c5a2a36c9381b30aa8c503f634db1e4a9643
BeJ Park <bjpark0506@naver.com>
2021-09-14 10:47:54 +0900

    [09.14] JQuery selector

---
+++ 2_FE/day17/js/main.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     $('.ui-widget').click(function() {
+//         $('#name').hide();
+//         alert('indise html');
+//         console.log('click');
+//     });
+// });
+$("div>p").css("border", "3px dotted red");
+$(document).ready(function() {
+    $('.ui-widget').click(function() {
+        $('#name').hide();
+    });
+
+});
