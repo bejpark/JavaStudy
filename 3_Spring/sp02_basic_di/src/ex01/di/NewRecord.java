f620346d8474583c730e2dbd6e25d536b7225790
BeJ Park <bjpark0506@naver.com>
2021-09-16 15:04:11 +0900

    [09.16] is-a,has-a, di

---
+++ 3_Spring/sp02_basic_di/src/ex01/di/NewRecord.java
@@ -0,0 +1,58 @@
+package ex01.di;
+
+public class NewRecord { //DTO 클래스
+	private int kor,eng,math,com;
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+	
+	public NewRecord() {
+		
+	}
+
+	public NewRecord(int kor, int eng, int math, int com) {
+		super();
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+	
+	public int total() {
+		return this.kor+this.eng+this.math+this.com;
+	}
+	public float avg() {
+		return total()/4.f;
+	}
+	
+
+}
---
+++ 3_Spring/sp02_basic_di/src/ex01/di/NewRecordView.java
@@ -0,0 +1,31 @@
+package ex01.di;
+
+public class NewRecordView {
+	private NewRecord record;
+//	public NewRecordView(int kor,int math,int eng, int com) {
+//		record = new NewRecord(kor,eng,math,com);
+//		
+//	}
+	
+	//setter로 주입받기
+	
+	public void print() {
+		System.out.println(record.total()+"/"+record.avg());
+		System.out.printf("kor : %d\n", record.getKor());
+		System.out.printf("eng : %d\n", record.getEng());
+		System.out.printf("math : %d\n", record.getMath());
+		System.out.printf("com : %d\n", record.getCom());
+		System.out.printf("total : %d\n", record.total());
+		System.out.printf("avg : %d\n", record.avg());
+
+	}
+
+	public NewRecord getRecord() {
+		return record;
+	}
+
+	public void setRecord(NewRecord record) {
+		this.record = record;
+	}
+
+}
