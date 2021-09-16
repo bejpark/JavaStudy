927eade9526313d3732309c2d728134759e9c11b
BeJ Park <bjpark0506@naver.com>
2021-09-16 17:24:42 +0900

    [09.16] Spring bean xml

--- 3_Spring/sp02_basic_di/src/ex04/spring/di/MainEntry.java
+++ 3_Spring/sp02_basic_di/src/ex04/spring/di/MainEntry.java
@@ -1,11 +1,21 @@
 package ex04.spring.di;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainEntry {
 
 	public static void main(String[] args) {
-		
-		
+		//ApplicationContext context = new ClassPathXmlApplicationContext("ex04/spring/di/appCtx.xml"); 
 
+		ApplicationContext context = new ClassPathXmlApplicationContext("appCtx.xml"); //bean생성 설정 파일 경로와 파일명 지정한
+		
+		//interface 타입으로 설정 가능 
+		IRecordView view = (IRecordViewImpl)context.getBean("view"); //getBean(bean 네임)메소드로 객체 가져온다.
+//		IRecordView view = context.getBean("view",IRecordViewImpl.class); //getBean(bean 네임)메소드로 객체 가져온다.
+		view.input();
+
+		view.print();
 	}
 
 }
