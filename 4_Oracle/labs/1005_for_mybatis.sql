555582faaaec336994d1493d49523b4ee48e934d
BeJ Park <bjpark0506@naver.com>
2021-10-06 12:26:57 +0900

    [10.06] MyBatis 게시판

--- 4_Oracle/labs/1005_for_mybatis.sql
+++ 4_Oracle/labs/1005_for_mybatis.sql
@@ -15,3 +15,18 @@ create SEQUENCE seq_board;
 insert into board values(seq_board.nextVal,'title2','writer2','contents2...',sysdate,0);
 
 commit;
+
+CREATE TABLE spring_board
+(
+  num NUMBER, 
+  NAME VARCHAR2(50), 
+  email  VARCHAR2(50), 
+  pwd  VARCHAR2(50), 
+  subject  VARCHAR2(50), 
+  content  VARCHAR2(2000), 
+  regdate  date,
+  hit  NUMBER, 
+  parent  NUMBER, 
+  sort  NUMBER, 
+  tab  NUMBER
+);
