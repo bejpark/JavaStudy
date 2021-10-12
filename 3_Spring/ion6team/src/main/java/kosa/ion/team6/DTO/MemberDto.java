dbfa7a67a3e30324da5c620fa9d02dcf6e1fb2b6
BeJ Park <bjpark0506@naver.com>
2021-10-12 16:18:12 +0900

    [ION6] Login

---
+++ 3_Spring/ion6team/src/main/java/kosa/ion/team6/DTO/MemberDto.java
@@ -0,0 +1,15 @@
+package kosa.ion.team6.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberDto {
+    private String name;
+    private String email;
+    private String password;
+    private String birth;
+    private String address;
+    private String addressDetail;
+    
+}
