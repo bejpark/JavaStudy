19e21d9e88d3dc30cbb326256bc52aac0a045a66
BeJ Park <bjpark0506@naver.com>
2021-10-13 11:49:22 +0900

    [10.13] Vue Basics

---
+++ 5_Vue/day33/readme.txt
@@ -0,0 +1,2 @@
+1. vue create 프로젝트 이름 (vue-form)
+2. default 옵션 선택 후 생성
---
+++ 5_Vue/day33/vueex01/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <dir>
+    <form>
+      <dir>
+        <label for="username">id: </label>
+        <input type="text" id="username">
+      </dir>
+      <div>
+        <label for="password">pwd: </label>
+        <input type="password" id="password">
+      </div>
+      <button>로그인</button>
+    </form>
+  </dir>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
