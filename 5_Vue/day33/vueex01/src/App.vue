a50dad126fc51e5a99e78f008ee718427b091848
BeJ Park <bjpark0506@naver.com>
2021-10-13 17:14:37 +0900

    [10.13] Vue

--- 5_Vue/day33/vueex01/src/App.vue
+++ 5_Vue/day33/vueex01/src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <dir>
-    <form>
-      <dir>
-        <label for="username">id: </label>
-        <input type="text" id="username">
-      </dir>
+    <form v-on:click="submitForm">
       <div>
-        <label for="password">pwd: </label>
-        <input type="password" id="password">
+        <label for="username">ID: </label>
+        <input type="text" id="username" v-model="username">
       </div>
-      <button>로그인</button>
+       <div>
+        <label for="password">PW: </label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit">로그인</button>
     </form>
   </dir>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('submitte')
 
-}
+    },
+  },
+};
 </script>
 
 <style>
