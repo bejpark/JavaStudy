ec810f83aebaabbff43ed43a5a7ed3a442c67dab
BeJ Park <bjpark0506@naver.com>
2021-10-15 15:06:08 +0900

    [10.15] Vue Event

---
+++ 5_Vue/day33/vueex01/src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="black-bg">
+      <div class="white-bg">
+        <img :src=rooms[roomNo].image>
+        <h4>{{rooms[roomNo].title}}</h4>
+        <p>{{rooms[roomNo].content}}</p>
+        <p> {{rooms[roomNo].price}}만원</p>
+        <Discount />
+        <button @click="isModalOpen">닫기</button>
+      </div>
+    </div>
+</template>
+
+<script>
+import Discount from './Discount.vue'
+export default {
+    name:'Modal',
+    components:{
+      Discount
+    },
+    props:{
+      rooms:Array,
+      roomNo:Number,
+      
+    },
+    methods:{
+      isModalOpen(){
+        this.$emit("child",false)
+      }
+    }
+}
+</script>
+
+<style>
+.black-bg {
+    width: 100%;
+    height: 100%;
+    background: rgb(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
+  }
+
+  .white-bg {
+    width: 100%;
+
+    background-color: whitesmoke;
+    border-radius: 8px;
+    padding: 20px;
+
+  }
+</style>
